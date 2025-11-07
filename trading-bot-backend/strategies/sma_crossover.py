48370d0c5069a0e8103be0cd90c8255199369ab6
Bryant Velasquez <your_email@example.com>
2025-11-07 16:25:54 -0500

    refactor: made sma_crossover more readable by using new variable names

--- trading-bot-backend/strategies/sma_crossover.py
+++ trading-bot-backend/strategies/sma_crossover.py
@@ -11,9 +11,16 @@ class SMAStrategy:
         data["SMA_short"] = data["Close"].rolling(window=self.short_window).mean()
         data["SMA_long"] = data["Close"].rolling(window=self.long_window).mean()
 
-        if data["SMA_short"].iloc[-1] > data["SMA_long"].iloc[-1]:
-            return "BUY"
-        elif data["SMA_short"].iloc[-1] < data["SMA_long"].iloc[-1]:
-            return "SELL"
+        #Use to compare prev short/long and curr short/long
+        prev_short = data["SMA_short"].iloc[-2]
+        curr_short = data["SMA_short"].iloc[-1]
+        prev_long = data["SMA_long"].iloc[-2]
+        curr_long = data["SMA_long"].iloc[-1]
+
+
+        if prev_short < prev_long and curr_short > curr_long:
+            return "BUY" # short SMA crossed above long SMA
+        elif prev_short > prev_long and curr_short < curr_long :
+            return "SELL" # short SMA below long SMA
         else:
             return "HOLD"
